0e6619869860128c34b3325b9490f85d77a72b02
Kyle Shores <kyle.shores44@gmail.com>
2023-01-12 17:25:56 -0600

    wrote test

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,6 @@
 find_package(PkgConfig REQUIRED)
 
+################################################################################
 # json-fortran library
 find_path(JSON_INCLUDE_DIR json_module.mod
   DOC "json-fortran include directory (must include json_*.mod files)"
@@ -17,5 +18,20 @@ find_library(JSON_LIB jsonfortran
     /usr/local/lib64)
 include_directories(${JSON_INCLUDE_DIR})
 
+################################################################################
 # NetCDF library
 pkg_check_modules(netcdff IMPORTED_TARGET REQUIRED netcdf-fortran)
+
+################################################################################
+# google test
+include(FetchContent)
+FetchContent_Declare(googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG release-1.12.1
+  FIND_PACKAGE_ARGS NAMES GTest
+)
+
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(googletest)
