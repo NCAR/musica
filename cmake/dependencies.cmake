a22810f6409934d410770b040dc9f498f92f3876
Kyle Shores <kyle.shores44@gmail.com>
2023-04-03 15:05:34 -0600

    only requiring gtest if requested

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -24,14 +24,16 @@ pkg_check_modules(netcdff IMPORTED_TARGET REQUIRED netcdf-fortran)
 
 ################################################################################
 # google test
-include(FetchContent)
-FetchContent_Declare(googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG release-1.12.1
-  FIND_PACKAGE_ARGS NAMES GTest
-)
+if(ENABLE_TESTS)
+  include(FetchContent)
+  FetchContent_Declare(googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG release-1.12.1
+    FIND_PACKAGE_ARGS NAMES GTest
+  )
 
-set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
-set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+  set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+  set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
 
-FetchContent_MakeAvailable(googletest)
+  FetchContent_MakeAvailable(googletest)
+endif()
