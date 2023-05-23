d56c574211fd367bf4ab18d38f56c4a33810b97c
Kyle Shores <kyle.shores44@gmail.com>
2023-05-23 12:34:54 -0500

    saving progress

--- ccpp/micm/micm.F90
+++ ccpp/micm/micm.F90
@@ -6,30 +6,44 @@ module micm
    !> Interface to c reaction functions
    interface
 
-    ! Based on this: https://fortran-lang.discourse.group/t/iso-c-binding-interface-to-a-c-function-returning-a-string/527/6
-    ! and this https://community.intel.com/t5/Intel-Fortran-Compiler/Calling-a-C-function-and-returning-a-string/m-p/1179874/highlight/true#M148382
-   subroutine getAllComponentVersions( str, irc ) bind(C, name="c_getAllComponentVersions" )
-        import :: c_char, c_int
+      ! Based on this: https://fortran-lang.discourse.group/t/iso-c-binding-interface-to-a-c-function-returning-a-string/527/6
+      ! and this https://community.intel.com/t5/Intel-Fortran-Compiler/Calling-a-C-function-and-returning-a-string/m-p/1179874/highlight/true#M148382
+      subroutine getAllComponentVersions( str, irc ) bind(C, name="c_getAllComponentVersions" )
+         import :: c_char, c_int
 
-        ! Argument list
-        character(kind=c_char,len=:), allocatable, intent(out) :: str
-        integer(c_int), intent(inout)                          :: irc
-    end subroutine getAllComponentVersions
+         ! Argument list
+         character(kind=c_char,len=:), pointer, intent(out) :: str
+         integer(c_int), intent(inout)                      :: irc
+      end subroutine getAllComponentVersions
+
+      ! Interface to C function returning an integer
+      function c_returnInteger() bind(C, name="c_returnInteger") result(num)
+         use iso_c_binding
+         implicit none
+         integer(c_int) :: num
+      end function c_returnInteger
 
    end interface
 
 contains
 
    subroutine micm_init()
-    use iso_c_binding
-    character(kind=c_char,len=:), allocatable :: versions
-    integer(c_int) :: irc
- 
-    ! Call the C function
-    call getAllComponentVersions(versions, irc)
- 
-    ! Print the result
-    print *, "Result:", NEW_LINE('a'), versions
+      use iso_c_binding
+      character(kind=c_char,len=:), pointer :: versions
+      integer(c_int) :: irc, ret
+
+      ! Call the C function
+      call getAllComponentVersions(versions, irc)
+
+      ! Print the result
+      print *, "Result:"
+      print *, trim(versions)
+
+      ret = c_returnInteger()
+      print *, "Returned Integer:", ret
+
+      ! Deallocate the allocated memory for the string
+      deallocate(versions)
    end subroutine micm_init
 
 end module micm
--- src/c_fortran_interface/interface_component_versions.c
+++ src/c_fortran_interface/interface_component_versions.c
@@ -1,6 +1,7 @@
 #include <ISO_Fortran_binding.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <musica/component_versions.h>
 
@@ -8,6 +9,7 @@
 extern "C" {
 #endif
   void c_getAllComponentVersions(CFI_cdesc_t* Fstr, int *ret);
+  int c_returnInteger();
 #ifdef __cplusplus
 }
 #endif
@@ -15,9 +17,17 @@ extern "C" {
 void c_getAllComponentVersions(CFI_cdesc_t* Fstr, int *ret) {
   char* versions = getAllComponentVersions();
   size_t len = strlen(versions);
+  printf("Length: %zu\n", len);
+  printf("Versions: %s\n", versions);
   *ret = CFI_allocate(Fstr, (CFI_index_t *)0, (CFI_index_t *)0, len);
   if (*ret == 0) {
+    printf("CFI_allocate succeeded\n");
     memcpy(Fstr->base_addr, versions, len);
   }
   free(versions);
+}
+
+int c_returnInteger() {
+  // Return an integer value to Fortran
+  return 42;
 }
