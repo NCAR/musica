2df0d15e0d79ca22d522ddb7957190db2636450e
Kyle Shores <kyle.shores44@gmail.com>
2023-05-25 09:51:52 -0500

    adding arguments to micm ccpp stuff

--- ccpp/micm/micm.F90
+++ ccpp/micm/micm.F90
@@ -3,6 +3,8 @@ module micm
 
    implicit none
 
+   procedure(solver), pointer :: fsolver
+
    interface
       type(c_funptr) function get_solver(filepath) bind(c)
          import :: c_char, c_funptr
@@ -21,36 +23,60 @@ module micm
 
 contains
 
-   subroutine micm_init()
+   !> \section arg_table_micm_init Argument Table
+   !! \htmlinclude micm_init.html
+   subroutine micm_init(filepath, errmsg, errflg)
+      ! Arguments
+      character(len=512), intent(out) :: errmsg
+      integer, intent(out) :: errflg
+      character(len=*), intent(in) :: filepath
+
+      ! Local variables
       type(c_funptr) :: csolver_func_pointer
-      procedure(solver), pointer :: fsolver
-      character(len=*, kind=c_char), parameter :: filepath = "hello.txt"
 
-      real(c_double), dimension(:), allocatable :: state
-      integer(c_int64_t) :: state_size = 5
-      integer(c_int64_t) :: time_step = 1
+      ! Convert Fortran character array to C character array
+      character(len=len(filepath)+1, kind=c_char) :: c_filepath
+      c_filepath = transfer(filepath, c_filepath)
 
-      allocate(state(state_size))
-
-      state = 1
-
-      csolver_func_pointer = get_solver(filepath)
+      csolver_func_pointer = get_solver(c_filepath)
       call c_f_procpointer(csolver_func_pointer, fsolver)
-
-      call fsolver(state, state_size, time_step)
    end subroutine micm_init
 
+   !> \section arg_table_micm_timestep_init Argument Table
+   !! \htmlinclude micm_timestep_init.html
+   subroutine micm_timestep_init(errmsg, errflg)
+      character(len=512),intent(out):: errmsg
+      integer,           intent(out):: errflg
 
-   subroutine micm_timestep_init
    end subroutine micm_timestep_init
 
-   subroutine micm_run
+   !> \section arg_table_micm_run Argument Table
+   !! \htmlinclude micm_run.html
+   subroutine micm_run(state, state_size, time_step, errmsg, errflg)
+      character(len=512),intent(out):: errmsg
+      integer,           intent(out):: errflg
+
+      real(c_double), dimension(:), allocatable :: state
+      integer(c_int64_t) :: state_size 
+      integer(c_int64_t) :: time_step
+
+      call fsolver(state, state_size, time_step)
    end subroutine micm_run
 
-   subroutine micm_timestep_final
+   !> \section arg_table_micm_timestep_final Argument Table
+   !! \htmlinclude micm_timestep_final.html
+   subroutine micm_timestep_final(errmsg, errflg)
+      character(len=512),intent(out):: errmsg
+      integer,           intent(out):: errflg
+
    end subroutine micm_timestep_final
 
-   subroutine micm_final
+   !> \section arg_table_micm_final Argument Table
+   !! \htmlinclude micm_final.html
+   subroutine micm_final(errmsg, errflg)
+      character(len=512),intent(out):: errmsg
+      integer,           intent(out):: errflg
+
    end subroutine micm_final
 
 end module micm
