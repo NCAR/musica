3736ddc2c93cd7d54e37f9328a23f35beb82dc86
Jiwon Gim <jiwongim@ucar.edu>
2023-07-21 17:44:33 -0600

    add fortran interface

--- src/fortran_interface/micm/interface.F90
+++ src/fortran_interface/micm/interface.F90
@@ -1,18 +1,19 @@
-!< \section arg_table_micm
-!! \htmlinclude micm.html
+module micm
 
    interface
 
-      type(c_funptr) function get_solver(filepath) bind(c)
-         import :: c_char, c_funptr
-         character(len=1, kind=c_char), dimension(*), intent(in) :: filepath
-      end function get_solver
+!      type(c_funptr) function get_solver(filepath) bind(c)
+!         import :: c_char, c_funptr
+!         character(len=1, kind=c_char), dimension(*), intent(in) :: filepath
+!      end function get_solver
 
-      subroutine solver(state, state_size, time_step) bind(c)
-         import :: c_ptr, c_double, c_int64_t
-         real(c_double), dimension(*) :: state
-         integer(c_int64_t), value :: state_size
-         integer(c_int64_t), value :: time_step
-      end subroutine
+!      subroutine solver(state, state_size, time_step) bind(c)
+!         import :: c_ptr, c_double, c_int64_t
+!         real(c_double), dimension(*) :: state
+!         integer(c_int64_t), value :: state_size
+!         integer(c_int64_t), value :: time_step
+!      end subroutine
 
    end interface
+
+end module micm
