abf9370a0685af8482fd278b3aa3fd840897732a
Jiwon Gim <jiwongim@ucar.edu>
2023-07-31 14:58:50 -0600

    update musica/musica cmake

--- musica/cmake/musica_util.cmake
+++ musica/cmake/musica_util.cmake
@@ -6,10 +6,8 @@ function(checkout_submodules)
       option(GIT_SUBMODULE "Check submodules during build" ON)
       if(GIT_SUBMODULE)
           message(STATUS "Submodule update")
-          message("   * ${CMAKE_CURRENT_SOURCE_DIR} : current")
           execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-                        #   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                        # WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                          WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                           RESULT_VARIABLE GIT_SUBMOD_RESULT)
           if(NOT GIT_SUBMOD_RESULT EQUAL "0")
               message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
