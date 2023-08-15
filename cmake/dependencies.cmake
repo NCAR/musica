997279b61d880bc7de51d66a604db4eab1c33bb4
Jiwon Gim <jiwongim@ucar.edu>
2023-08-15 13:40:14 -0600

    cmake dependent options for musica

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -29,7 +29,6 @@ if(ENABLE_TESTS)
   FetchContent_Declare(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG be03d00f5f0cc3a997d1a368bee8a1fe93651f48
-    # FIND_PACKAGE_ARGS NAMES GTest
   )
 
   set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
