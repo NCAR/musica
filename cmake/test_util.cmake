adc6ab66469ee251031172ed46abc0bbf1ac57f4
Kyle Shores <kyle.shores44@gmail.com>
2024-03-11 08:22:21 -0500

    including tuvx that can be built with gcc 13

--- cmake/test_util.cmake
+++ cmake/test_util.cmake
@@ -8,7 +8,7 @@ endif()
 ################################################################################
 # build and add a standard test linked to musica
 
-function(create_standard_test)
+function(create_standard_test_fortran)
   set(prefix TEST)
   set(singleValues NAME WORKING_DIRECTORY)
   set(multiValues SOURCES LIBRARIES)
@@ -16,7 +16,7 @@ function(create_standard_test)
   cmake_parse_arguments(${prefix} " " "${singleValues}" "${multiValues}" ${ARGN})
 
   add_executable(test_${TEST_NAME} ${TEST_SOURCES})
-  target_link_libraries(test_${TEST_NAME} PUBLIC musica::musica)
+  target_link_libraries(test_${TEST_NAME} PUBLIC musica::musica-fortran)
 
   # link additional libraries
   foreach(library ${TEST_LIBRARIES})
@@ -32,7 +32,7 @@ function(create_standard_test)
   endif()
 
   add_musica_test(${TEST_NAME} test_${TEST_NAME} "" ${TEST_WORKING_DIRECTORY})
-endfunction(create_standard_test)
+endfunction(create_standard_test_fortran)
 
 function(create_standard_test_cxx)
   set(prefix TEST)
@@ -64,23 +64,23 @@ function(add_musica_test test_name test_binary test_args working_dir)
              COMMAND ${test_binary} ${test_args}
              WORKING_DIRECTORY ${working_dir})
   endif()
-  set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=1 --trace-children=yes --leak-check=full --gen-suppressions=all ${MEMCHECK_SUPPRESS}")
-  set(memcheck "${MEMORYCHECK_COMMAND} ${MEMORYCHECK_COMMAND_OPTIONS}")
-  separate_arguments(memcheck)
-  if(MUSICA_ENABLE_MPI AND MEMORYCHECK_COMMAND AND MUSICA_ENABLE_MEMCHECK)
-    add_test(NAME memcheck_${test_name}
-      COMMAND mpirun -v -np 2 ${memcheck} ${CMAKE_BINARY_DIR}/${test_binary} ${test_args}
-             WORKING_DIRECTORY ${working_dir})
+  # set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=1 --trace-children=yes --leak-check=full --gen-suppressions=all ${MEMCHECK_SUPPRESS}")
+  # set(memcheck "${MEMORYCHECK_COMMAND} ${MEMORYCHECK_COMMAND_OPTIONS}")
+  # separate_arguments(memcheck)
+  # if(MUSICA_ENABLE_MPI AND MEMORYCHECK_COMMAND AND MUSICA_ENABLE_MEMCHECK)
+  #   add_test(NAME memcheck_${test_name}
+  #     COMMAND mpirun -v -np 2 ${memcheck} ${CMAKE_BINARY_DIR}/${test_binary} ${test_args}
+  #            WORKING_DIRECTORY ${working_dir})
     
-    # add dependency between memcheck and previous test
-    # https://stackoverflow.com/a/66931930/5217293
-    set_tests_properties(${test_name} PROPERTIES FIXTURES_SETUP f_${test_name})
-    set_tests_properties(memcheck_${test_name} PROPERTIES FIXTURES_REQUIRED f_${test_name})
-  elseif(MEMORYCHECK_COMMAND AND MUSICA_ENABLE_MEMCHECK)
-    add_test(NAME memcheck_${test_name}
-             COMMAND ${memcheck} ${CMAKE_BINARY_DIR}/${test_binary} ${test_args}
-             WORKING_DIRECTORY ${working_dir})
-  endif()
+  #   # add dependency between memcheck and previous test
+  #   # https://stackoverflow.com/a/66931930/5217293
+  #   set_tests_properties(${test_name} PROPERTIES FIXTURES_SETUP f_${test_name})
+  #   set_tests_properties(memcheck_${test_name} PROPERTIES FIXTURES_REQUIRED f_${test_name})
+  # elseif(MEMORYCHECK_COMMAND AND MUSICA_ENABLE_MEMCHECK)
+  #   add_test(NAME memcheck_${test_name}
+  #            COMMAND ${memcheck} ${CMAKE_BINARY_DIR}/${test_binary} ${test_args}
+  #            WORKING_DIRECTORY ${working_dir})
+  # endif()
 endfunction(add_musica_test)
 
 ################################################################################
