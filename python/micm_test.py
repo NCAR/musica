60186fe19a2d5e070a0ebec6e658e51112ca9ca1
Kyle Shores <kyle.shores44@gmail.com>
2024-03-25 06:18:12 -0500

    fixing python build

--- python/micm_test.py
+++ python/micm_test.py
@@ -10,16 +10,15 @@ pressure = 101253.3
 num_concentrations = 5
 concentrations = [0.75, 0.4, 0.8, 0.01, 0.02]
 
+print(concentrations)
 
-solver = musica.create_micm("/Users/alexgarza/Documents/CSCE482/musica/src/micm/configs/chapman")  
+solver = musica.create_micm("/Users/kshores/Documents/musica/configs/chapman")  
 musica.micm_solve(solver, time_step, temperature, pressure, concentrations)
+
+print(concentrations)
 
 assert concentrations[0] == 0.75
 assert concentrations[1] != 0.4
 assert concentrations[2] != 0.8
 assert concentrations[3] != 0.01
 assert concentrations[4] != 0.02
-
-
-
-
--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ class CMakeBuild(build_ext):
         # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
         # from Python.
         cmake_args = [
+            f"-DMUSICA_ENABLE_PYTHON_LIBRARY=ON",
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
@@ -54,9 +55,6 @@ class CMakeBuild(build_ext):
         # (needed e.g. to build for ARM OSx on conda-forge)
         if "CMAKE_ARGS" in os.environ:
             cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
-
-        # In this example, we pass in the version to C++. You might not need to.
-        cmake_args += [f"-DEXAMPLE_VERSION_INFO={self.distribution.get_version()}"]
 
         if self.compiler.compiler_type != "msvc":
             # Using Ninja-build since it a) is available as a wheel and b)
