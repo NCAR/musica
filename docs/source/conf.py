61b7c1ed1016d4393203fe021347749996f13bd6
David Fillmore <fillmore@ucar.edu>
2024-04-03 11:51:12 -0600

    Added conf.py from MICM docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,20 +10,20 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+import datetime
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'MUSICA'
-copyright = '2024, ACOM'
-author = 'ACOM'
+copyright = f'2024-{datetime.datetime.now().year}, NCAR/UCAR'
+author = 'NCAR/UCAR'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0'
-
+suffix = ''
+# the suffix is required. This is controlled by the dockerfile that builds the docs
+release = f'0.0.0'
 
 # -- General configuration ---------------------------------------------------
 
@@ -31,7 +31,13 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx_copybutton',
+  'sphinx_design',
+  'sphinxcontrib.bibtex',
 ]
+
+bibtex_bibfiles = ['references.bib']
+suppress_warnings = ["bibtex.missing_field"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,9 +53,23 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
+
+html_theme_options = {
+    "external_links": [],
+    "github_url": "https://github.com/NCAR/musica",
+    "navbar_end": ["version-switcher", "navbar-icon-links"],
+   "pygment_light_style": "tango",
+   "pygment_dark_style": "monokai"
+}
+
+html_css_files = [
+    'custom.css'
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_favicon = '_static/favicon/favicon.ico'
