5b499badffe00b97545fad197fed8ec8f49f6c64
Jiwon Gim <jiwongim@ucar.edu>
2024-05-13 16:58:31 -0600

    update git action version and make header comments consistent

--- include/musica/component_versions.h
+++ include/musica/component_versions.h
@@ -1,8 +1,6 @@
-/**
- * This file contains the version information for the different project modules
- * Copyright (C) 2023-2024 National Center for Atmospheric Research,
- * 
- * SPDX-License-Identifier: Apache-2.0* creating solvers, and solving the model.
+/* Copyright (C) 2023-2024 National Center for Atmospheric Research
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
 
--- include/musica/tuvx.hpp
+++ include/musica/tuvx.hpp
@@ -1,9 +1,9 @@
-/**
- * This file contains the defintion of the TUVX class, which represents a photolysis calculator
- * It also includes functions for creating and deleting TUVX instances with c binding
- * Copyright (C) 2023-2024 National Center for Atmospheric Research,
+ /* Copyright (C) 2023-2024 National Center for Atmospheric Research
  *
- * SPDX-License-Identifier: Apache-2.0* creating solvers, and solving the model.
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * This file contains the defintion of the TUVX class, which represents a photolysis calculator.
+ * It also includes functions for creating and deleting TUVX instances with c binding.
  */
 #pragma once
 
