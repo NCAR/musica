382a766113280a02ab98709de9ca121bd0f5b28a
Matt Dawson <mattdawson@ucar.edu>
2024-05-16 12:56:07 -0700

    in progress

--- src/test/unit/micm/micm.cpp
+++ src/test/unit/micm/micm.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 
 #include <micm/version.hpp>
+#include <musica/component_versions.hpp>
 
 TEST(ConnectToMICM, Version) {
   std::string version = micm::getMicmVersion();
   std::cout << version << std::endl;
+  char* versions = musica::getAllComponentVersions();
 }
