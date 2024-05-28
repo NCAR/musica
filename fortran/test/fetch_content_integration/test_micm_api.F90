078edc47a7be952c7f8d1d290a309e175c922260
David Fillmore <fillmore@ucar.edu>
2024-05-28 20:59:11 +0000

    Comment get_micm_version for now.

--- fortran/test/fetch_content_integration/test_micm_api.F90
+++ fortran/test/fetch_content_integration/test_micm_api.F90
@@ -2,8 +2,9 @@ program test_micm_api
 
   use, intrinsic :: iso_c_binding
   use, intrinsic :: ieee_arithmetic
-  use musica_micm, only: micm_t, get_micm_version
-  use musica_util, only: assert, error_t, mapping_t
+  ! use musica_micm, only: micm_t, get_micm_version
+  use musica_micm, only: micm_t
+  use musica_util, only: assert, error_t, mapping_t, string_t_c
 
 #include "micm/util/error.hpp"
 
@@ -43,7 +44,8 @@ contains
     num_user_defined_reaction_rates = 3
     user_defined_reaction_rates = (/ 0.1, 0.2, 0.3 /)
 
-    ! print *, "MICM version", get_micm_version()
+    ! micm_version = get_micm_version()
+    ! print *, "MICM version", micm_version
 
     write(*,*) "[test micm fort api] Creating MICM solver..."
     micm => micm_t(config_path, error)
