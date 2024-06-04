66d509132d0a4eaa9dc9c890e5f0d758a89d6efd
David Fillmore <fillmore@ucar.edu>
2024-06-04 14:56:39 -0600

    In test_micm_box_model, create micm_t solver.

--- fortran/test/fetch_content_integration/test_micm_box_model.F90
+++ fortran/test/fetch_content_integration/test_micm_box_model.F90
@@ -3,8 +3,8 @@ program test_micm_box_model
     use, intrinsic :: iso_c_binding
     use, intrinsic :: ieee_arithmetic
 
-    use musica_micm, only: micm_t
     use musica_util, only: error_t, string_t, mapping_t
+    use musica_micm, only: micm_t
 
     implicit none
 
@@ -14,11 +14,22 @@ contains
 
     subroutine box_model()
 
+        character(len=256) :: config_path
+
         real(c_double) :: time_step
         real(c_double) :: temperature
         real(c_double) :: pressure
 
         real(c_double), dimension(3) :: concentrations
+
+        type(error_t) :: error
+
+        type(micm_t), pointer :: micm
+
+        config_path = ""
+
+        write(*,*) "Creating MICM solver..."
+        micm => micm_t(config_path, error)
 
     end subroutine box_model
 
