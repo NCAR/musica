73f997d9b345f2180f17a54921fefe289807f632
David Fillmore <fillmore@ucar.edu>
2024-06-04 17:49:47 -0600

    Initialize some variables.

--- fortran/test/fetch_content_integration/test_micm_box_model.F90
+++ fortran/test/fetch_content_integration/test_micm_box_model.F90
@@ -20,13 +20,19 @@ contains
         real(c_double) :: temperature
         real(c_double) :: pressure
 
+        integer(c_int) :: num_concentrations = 3
         real(c_double), dimension(3) :: concentrations
 
         type(error_t) :: error
-
         type(micm_t), pointer :: micm
 
+        integer :: i
+
         config_path = ""
+
+        time_step = 200
+        temperature = 273.0
+        pressure = 1.0e5
 
         write(*,*) "Creating MICM solver..."
         micm => micm_t(config_path, error)
