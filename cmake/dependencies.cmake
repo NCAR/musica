409cfd69337089ce678655b1e1b4631261f31a92
Kyle Shores <kyle.shores44@gmail.com>
2024-06-12 10:03:04 -0500

    using a branch that sets PIC on tuvx

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -74,7 +74,7 @@ if (MUSICA_ENABLE_TUVX AND MUSICA_BUILD_C_CXX_INTERFACE)
   set(TUVX_INSTALL_INCLUDE_DIR ${MUSICA_INSTALL_INCLUDE_DIR} CACHE STRING "" FORCE)
 
   set_git_default(TUVX_GIT_REPOSITORY https://github.com/NCAR/tuv-x.git)
-  set_git_default(TUVX_GIT_TAG fbb453fc65a82dc6afd80f3a531eaad3cf66445e)
+  set_git_default(TUVX_GIT_TAG pic)
 
   FetchContent_Declare(tuvx
     GIT_REPOSITORY ${TUVX_GIT_REPOSITORY}
