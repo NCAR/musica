4c108390ed40094d0930047dea57309b9ae8109c
Jiwon Gim <jiwongim@ucar.edu>
2024-07-05 14:37:11 -0600

    micm git tag

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -51,7 +51,7 @@ endif()
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE)
 
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG musica-vector-ordered-rosenbrock) #TODO(jiwon) need to update this
+  set_git_default(MICM_GIT_TAG v3.5.0)
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
