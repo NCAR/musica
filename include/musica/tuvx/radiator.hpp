e0c3d219fc8bbfd388cd3c95a3646d2e3aa1e288
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2025-01-15 11:10:41 -0700

    Happy New Year (#247)

--- include/musica/tuvx/radiator.hpp
+++ include/musica/tuvx/radiator.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2023-2024 National Center for Atmospheric Research
+// Copyright (C) 2023-2025 National Center for Atmospheric Research
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
