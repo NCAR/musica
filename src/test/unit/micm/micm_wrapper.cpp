84b430b48552b1de8e286b0eb29b571ec7a516d9
Kyle Shores <kyle.shores44@gmail.com>
2025-03-06 09:06:08 -0600

    adding more tests

--- src/test/unit/micm/micm_wrapper.cpp
+++ src/test/unit/micm/micm_wrapper.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-TEST(MICMWrapper, CanParse) 
+TEST(MICMWrapper, CanParseChapman) 
 {
   musica::MICM micm;
   musica::Chemistry chemistry = micm.ReadConfiguration("configs/chapman", nullptr);
@@ -16,4 +16,20 @@ TEST(MICMWrapper, CanParse)
   EXPECT_EQ(chemistry.system.gas_phase_.species_[2].name_, "O");
   EXPECT_EQ(chemistry.system.gas_phase_.species_[3].name_, "O1D");
   EXPECT_EQ(chemistry.system.gas_phase_.species_[4].name_, "O3");
+}
+
+TEST(MICMWrapper, CanParseCBV) 
+{
+  musica::MICM micm;
+  musica::Chemistry chemistry = micm.ReadConfiguration("configs/carbon_bond_5", nullptr);
+  EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 67);
+  EXPECT_EQ(chemistry.processes.size(), 200);
+}
+
+TEST(MICMWrapper, CanParseTS1) 
+{
+  musica::MICM micm;
+  musica::Chemistry chemistry = micm.ReadConfiguration("configs/TS1", nullptr);
+  EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 211);
+  EXPECT_EQ(chemistry.processes.size(), 516);
 }
