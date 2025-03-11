cd4348395c09e19293cccee94495a7494072def9
Kyle Shores <kyle.shores44@gmail.com>
2025-03-11 09:24:11 -0500

    testing yaml v1

--- src/test/unit/micm/micm_wrapper.cpp
+++ src/test/unit/micm/micm_wrapper.cpp
@@ -57,48 +57,21 @@ TEST(MICMWrapper, DetectsInvalidConfigV0)
 
 TEST(MICMWrapper, CanParseChapmanV1) 
 {
-  musica::MICM micm;
-  musica::Error error;
-  musica::Chemistry chemistry = ReadConfiguration("configs/v1/chapman/config.json", &error);
-  ASSERT_TRUE(IsSuccess(error));
-  EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 5);
-  EXPECT_EQ(chemistry.processes.size(), 7);
-  EXPECT_EQ(chemistry.system.gas_phase_.species_[0].name_, "M");
-  EXPECT_NE(chemistry.system.gas_phase_.species_[0].parameterize_, nullptr);
-  EXPECT_EQ(chemistry.system.gas_phase_.species_[1].name_, "O2");
-  EXPECT_EQ(chemistry.system.gas_phase_.species_[2].name_, "O");
-  EXPECT_EQ(chemistry.system.gas_phase_.species_[3].name_, "O1D");
-  EXPECT_EQ(chemistry.system.gas_phase_.species_[4].name_, "O3");
+  std::vector<std::string> extensions = { ".json", ".yaml" };
+
+  for(const auto& extension : extensions)
+  {
+    musica::MICM micm;
+    musica::Error error;
+    musica::Chemistry chemistry = ReadConfiguration("configs/v1/chapman/config" + extension, &error);
+    ASSERT_TRUE(IsSuccess(error));
+    EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 5);
+    EXPECT_EQ(chemistry.processes.size(), 7);
+    EXPECT_EQ(chemistry.system.gas_phase_.species_[0].name_, "M");
+    EXPECT_NE(chemistry.system.gas_phase_.species_[0].parameterize_, nullptr);
+    EXPECT_EQ(chemistry.system.gas_phase_.species_[1].name_, "O2");
+    EXPECT_EQ(chemistry.system.gas_phase_.species_[2].name_, "O");
+    EXPECT_EQ(chemistry.system.gas_phase_.species_[3].name_, "O1D");
+    EXPECT_EQ(chemistry.system.gas_phase_.species_[4].name_, "O3");
+  }
 }
-
-// TEST(MICMWrapper, CanParseCBVV1) 
-// {
-//   musica::MICM micm;
-//   musica::Error error;
-//   musica::Chemistry chemistry = ReadConfiguration("configs/carbon_bond_5", &error);
-//   ASSERT_TRUE(IsSuccess(error));
-//   EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 67);
-//   EXPECT_EQ(chemistry.processes.size(), 200);
-//   DeleteError(&error);
-// }
-
-// TEST(MICMWrapper, CanParseTS1V1) 
-// {
-//   musica::MICM micm;
-//   musica::Error error;
-//   musica::Chemistry chemistry = ReadConfiguration("configs/TS1", &error);
-//   ASSERT_TRUE(IsSuccess(error));
-//   EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 210);
-//   EXPECT_EQ(chemistry.processes.size(), 547);
-//   DeleteError(&error);
-// }
-
-// TEST(MICMWrapper, DetectsInvalidConfigV1) 
-// {
-//   musica::MICM micm;
-//   musica::Error error;
-//   musica::Chemistry chemistry = ReadConfiguration("configs/invalid", &error);
-//   ASSERT_FALSE(IsSuccess(error));
-//   ASSERT_TRUE(IsError(error, MUSICA_ERROR_CATEGORY, MUSICA_ERROR_CODE_CONFIG_PARSE_FAILED));
-//   DeleteError(&error);
-// }
