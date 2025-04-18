913c2d693a5e867a0a0b022dc62fe58ec6803b8c
Kyle Shores <kshores@ucar.edu>
2025-04-18 11:49:30 -0600

    adding test

--- src/test/unit/micm/micm_wrapper.cpp
+++ src/test/unit/micm/micm_wrapper.cpp
@@ -5,35 +5,52 @@
 
 #include <iostream>
 
+void DoChemistry(musica::MICMSolver solver_type)
+{
+  musica::Chemistry chemistry = musica::ReadConfiguration("configs/analytical");
+  musica::MICM micm = musica::MICM(chemistry, solver_type, 1);
+  musica::State state = musica::State(micm);
+
+  std::vector<double> initial_concnetrations = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+  state.SetOrderedConcentrations(initial_concnetrations);
+  state.SetConditions({ {.temperature_ = 298.15, .pressure_ = 101325.0 } });
+
+  double time_step = 60;
+  musica::SolverResultStats solver_stats;
+  musica::String solver_state;
+  micm.Solve(&micm, &state, time_step, &solver_state, &solver_stats);
+  EXPECT_EQ(std::string(solver_state.value_), std::string("Converged"));
+  EXPECT_NE(state.GetOrderedConcentrations()[0], initial_concnetrations[0]);
+  EXPECT_NE(state.GetOrderedConcentrations()[1], initial_concnetrations[1]);
+  EXPECT_NE(state.GetOrderedConcentrations()[2], initial_concnetrations[2]);
+  EXPECT_NE(state.GetOrderedConcentrations()[3], initial_concnetrations[3]);
+  EXPECT_NE(state.GetOrderedConcentrations()[4], initial_concnetrations[4]);
+  EXPECT_NE(state.GetOrderedConcentrations()[5], initial_concnetrations[5]);
+}
+
 TEST(MICMWrapper, CanCreateRosenbrock)
 {
-  musica::Chemistry chemistry = musica::ReadConfiguration("configs/chapman");
-  musica::MICM micm = musica::MICM(chemistry, musica::MICMSolver::Rosenbrock, 1);
+  DoChemistry(musica::MICMSolver::Rosenbrock);
 }
 
 TEST(MICMWrapper, CanCreateRosenbrockStandardOrder)
 {
-  musica::Chemistry chemistry = musica::ReadConfiguration("configs/chapman");
-  musica::MICM micm = musica::MICM(chemistry, musica::MICMSolver::RosenbrockStandardOrder, 1);
+  DoChemistry(musica::MICMSolver::RosenbrockStandardOrder);
 }
 
 TEST(MICMWrapper, CanCreateBackwardEuler)
 {
-  musica::Chemistry chemistry = musica::ReadConfiguration("configs/chapman");
-  musica::MICM micm = musica::MICM(chemistry, musica::MICMSolver::BackwardEuler, 1);
+  DoChemistry(musica::MICMSolver::BackwardEuler);
 }
 
 TEST(MICMWrapper, CanCreateBackwardEulerStandardOrder)
 {
-  musica::Chemistry chemistry = musica::ReadConfiguration("configs/chapman");
-  musica::MICM micm = musica::MICM(chemistry, musica::MICMSolver::BackwardEulerStandardOrder, 1);
+  DoChemistry(musica::MICMSolver::BackwardEulerStandardOrder);
 }
 
 #ifdef MUSICA_ENABLE_CUDA
 TEST(MICMWrapper, CanCreateCudaRosenbrock)
 {
-    musica::Chemistry chemistry = musica::ReadConfiguration("configs/chapman");
-    musica::MICM micm = musica::MICM(chemistry, musica::MICMSolver::CudaRosenbrock, 1);
-    musica::State state = musica::State(micm);
+  DoChemistry(musica::MICMSolver::CudaRosenbrock);
 }
 #endif
