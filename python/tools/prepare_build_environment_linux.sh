b6a3173a4d674c6cf86c34e1bd2c88d0450761a6
Kyle Shores <kyle.shores44@gmail.com>
2025-04-25 15:13:43 -0500

    using proper linux commands

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -3,7 +3,16 @@
 set -e
 set -x
 
-CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
-curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_537.13_windows.exe
-./cuda.exe -s nvcc_12.2 cudart_12.2 cublas_dev_12.2 curand_dev_12.2
-rm cuda.exe
+# Install CUDA 12.2:
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+# error mirrorlist.centos.org doesn't exists anymore.
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
+yum install --setopt=obsoletes=0 -y \
+    cuda-nvcc-12-2-12.2.140-1 \
+    cuda-cudart-devel-12-2-12.2.140-1 \
+    libcurand-devel-12-2-10.3.3.141-1 \
+    libcublas-devel-12-2-12.2.5.6-1 \
+    libnccl-devel-2.19.3-1+cuda12.2
+ln -s cuda-12.2 /usr/local/cuda
