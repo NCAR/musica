59f529101905b033c71a423cd39cc2aeafc2efab
Kyle Shores <kyle.shores44@gmail.com>
2025-04-28 17:13:07 -0500

    trying choco again

--- python/tools/prepare_build_environment_windows.sh
+++ python/tools/prepare_build_environment_windows.sh
@@ -3,12 +3,14 @@
 set -e
 set -x
 
-CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
-curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_537.13_windows.exe
-./cuda.exe -s nvcc_12.2 cudart_12.2 cublas_dev_12.2 curand_dev_12.2
-rm cuda.exe
+# CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
+# curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_537.13_windows.exe
+# ./cuda.exe -s nvcc_12.2 cudart_12.2 cublas_dev_12.2 curand_dev_12.2
+# rm cuda.exe
 
-export CUDA_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
-export PATH="$CUDA_PATH/bin:$PATH"
+# export CUDA_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
+# export PATH="$CUDA_PATH/bin:$PATH"
 
 # choco install cuda 
+
+choco install cuda --version=12.2
