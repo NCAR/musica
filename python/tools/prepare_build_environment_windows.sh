dfb3048d01c557b4c943fa4a1f8824842478849b
Kyle Shores <kyle.shores44@gmail.com>
2025-04-29 09:53:29 -0500

    trying local installer again

--- python/tools/prepare_build_environment_windows.sh
+++ python/tools/prepare_build_environment_windows.sh
@@ -3,15 +3,17 @@
 set -e
 set -x
 
-# CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
+CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.8"
 # curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_537.13_windows.exe
-# ./cuda.exe -s nvcc_12.2 cudart_12.2 cublas_dev_12.2 curand_dev_12.2
-# rm cuda.exe
+curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda_12.8.1_572.61_windows.exe
+./cuda.exe -s nvcc_12.8 cudart_12.8 cublas_dev_12.8 curand_dev_12.8
+rm cuda.exe
 
-# export CUDA_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2"
-# export PATH="$CUDA_PATH/bin:$PATH"
+export CUDA_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.8"
+export PATH="$CUDA_PATH/bin:$PATH"
 
-choco install cuda 
+# choco install cuda 
 
-ls "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
+ls $CUDA_PATH
+ls $CUDA_PATH/bin
 which nvcc
