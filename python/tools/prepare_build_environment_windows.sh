4e7dba072d7647459f4e96d81346cb66566d3b9d
Kyle Shores <kyle.shores44@gmail.com>
2025-04-29 11:07:13 -0500

    trying to install extensions

--- python/tools/prepare_build_environment_windows.sh
+++ python/tools/prepare_build_environment_windows.sh
@@ -6,11 +6,14 @@ set -x
 CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.8"
 # curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_537.13_windows.exe
 curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda_12.8.1_572.61_windows.exe
-./cuda.exe -s nvcc_12.8 cudart_12.8 cublas_dev_12.8 curand_dev_12.8
+./cuda.exe -s nvcc_12.8 cudart_12.8 cublas_dev_12.8 curand_dev_12.8 visual_studio_integration
 rm cuda.exe
 
 export CUDA_PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.8"
 export PATH="$CUDA_PATH/bin:$PATH"
+
+ls "c/Program Files/NVIDIA GPU Computing Toolkit/CUDA\v11.7/extras/visual_studio_integration/MSBuildExtensions"
+ls "c/Program Files (x86)/Microsoft Visual Studio/"
 
 # choco install cuda 
 
