f5e711564ac8b2701739921c0a28f1401b95f802
Jian Sun <sunjian@ucar.edu>
2025-05-09 21:56:09 -0600

    update micm tag

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -68,7 +68,7 @@ endif()
 
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE)
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG 9a218682) 
+  set_git_default(MICM_GIT_TAG b51aefbc) 
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
