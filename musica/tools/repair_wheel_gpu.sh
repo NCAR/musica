606e997a3c0f2ae4bf8fa0c204d1d21fe35d7e91
Kyle Shores <kyle.shores44@gmail.com>
2025-05-21 08:23:52 -0500

    separating the linux build options, and putting more build options into pyproject.toml

---
+++ musica/tools/repair_wheel_gpu.sh
@@ -0,0 +1,19 @@
+# musica/tools/repair_wheel_gpu.sh
+#!/usr/bin/env bash
+set -e
+
+auditwheel repair --exclude libcublas --exclude libcublasLt --exclude libcudart -w "$2" "$1"
+
+for whl in "$2"/*.whl; do
+  tmpdir=$(mktemp -d)
+  unzip -q "$whl" -d "$tmpdir"
+  echo "Before patchelf:"
+  readelf -d "$tmpdir"/_musica*.so
+  patchelf --remove-rpath "$tmpdir"/_musica*.so
+  patchelf --set-rpath "\$ORIGIN:\$ORIGIN/../../nvidia/cublas/lib:\$ORIGIN/../../nvidia/cuda_runtime/lib" --force-rpath "$tmpdir"/_musica*.so
+  echo "After patchelf:"
+  readelf -d "$tmpdir"/_musica*.so
+  zip -qr "${whl%.whl}.patched.whl" -j "$tmpdir"
+  mv "${whl%.whl}.patched.whl" "$whl"
+  rm -rf "$tmpdir"
+done
