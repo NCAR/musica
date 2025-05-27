9483cb424762eda5c3328ae561d14b13a0be8421
Kyle Shores <kyle.shores44@gmail.com>
2025-05-27 09:58:00 -0500

    repairing cuda so file names with patchelf

--- musica/tools/repair_wheel_gpu.sh
+++ musica/tools/repair_wheel_gpu.sh
@@ -9,8 +9,13 @@ for whl in "$2"/*.whl; do
   tree "$tmpdir"
   echo "Before patchelf:"
   readelf -d "$tmpdir"/_musica*.so
+  # Use patchelf to fix the rpath and library dependencies
   patchelf --remove-rpath "$tmpdir"/_musica*.so
   patchelf --set-rpath "\$ORIGIN:\$ORIGIN/../../nvidia/cublas/lib:\$ORIGIN/../../nvidia/cuda_runtime/lib" --force-rpath "$tmpdir"/_musica*.so
+  # these may need to be periodically updated
+  patchelf --replace-needed libcudart-b5a066d7.so.12.2.140 libcudart.so.12 _musica*.so
+  patchelf --replace-needed libcublas-e779a79d.so.12.2.5.6 libcublas.so.12 _musica*.so
+  patchelf --replace-needed libcublasLt-fbfbc8a1.so.12.2.5.6 libcublasLt.so.12 _musica*.so
   # Remove bundled CUDA libraries
   rm -f "$tmpdir"/musica.libs/libcudart-*.so*
   rm -f "$tmpdir"/musica.libs/libcublas-*.so*
