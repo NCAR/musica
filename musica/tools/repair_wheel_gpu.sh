9427a2fe7a5ce62724bc2256ba2050410037ec61
Kyle Shores <kyle.shores44@gmail.com>
2025-05-27 10:10:15 -0500

    correct so file path

--- musica/tools/repair_wheel_gpu.sh
+++ musica/tools/repair_wheel_gpu.sh
@@ -13,9 +13,9 @@ for whl in "$2"/*.whl; do
   patchelf --remove-rpath "$tmpdir"/_musica*.so
   patchelf --set-rpath "\$ORIGIN:\$ORIGIN/../../nvidia/cublas/lib:\$ORIGIN/../../nvidia/cuda_runtime/lib" --force-rpath "$tmpdir"/_musica*.so
   # these may need to be periodically updated
-  patchelf --replace-needed libcudart-b5a066d7.so.12.2.140 libcudart.so.12 _musica*.so
-  patchelf --replace-needed libcublas-e779a79d.so.12.2.5.6 libcublas.so.12 _musica*.so
-  patchelf --replace-needed libcublasLt-fbfbc8a1.so.12.2.5.6 libcublasLt.so.12 _musica*.so
+  patchelf --replace-needed libcudart-b5a066d7.so.12.2.140 libcudart.so.12 "$tmpdir"/_musica*.so
+  patchelf --replace-needed libcublas-e779a79d.so.12.2.5.6 libcublas.so.12 "$tmpdir"/_musica*.so
+  patchelf --replace-needed libcublasLt-fbfbc8a1.so.12.2.5.6 libcublasLt.so.12 "$tmpdir"/_musica*.so
   # Remove bundled CUDA libraries
   rm -f "$tmpdir"/musica.libs/libcudart-*.so*
   rm -f "$tmpdir"/musica.libs/libcublas-*.so*
