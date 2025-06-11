018fd1a14de161680cee1db9d85d19de8316eccb
Kyle Shores <kyle.shores44@gmail.com>
2025-06-11 09:11:16 -0500

    common bindings

--- musica/gpu_binding.cpp
+++ musica/gpu_binding.cpp
@@ -2,20 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <pybind11/pybind11.h>
+#include "binding_common.hpp"
 
-namespace py = pybind11;
-
-void bind_musica(py::module_ &);
-void bind_mechanism_configuration(py::module_ &);
-void bind_cuda(py::module_ &);
-
-// Wraps micm.cpp
 PYBIND11_MODULE(_musica_gpu, m)
 {
-  py::module_ core = m.def_submodule("_core", "Wrapper classes for MUSICA C library structs and functions");
-  py::module_ mechanism_configuration = m.def_submodule("_mechanism_configuration", "Wrapper classes for Mechanism Configuration library structs and functions");
-
-  bind_musica(core);
-  bind_mechanism_configuration(mechanism_configuration);
-  bind_cuda(core);
+  bind_all(m);
 }
