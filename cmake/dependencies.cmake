49209021824046636ea76bbccca4f8ef996da72e
Matt Dawson <mattdawson@ucar.edu>
2025-06-13 12:29:08 -0700

    update micm commit

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -73,7 +73,7 @@ endif()
 
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE)
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG 39941a7a63916d8d3d343154759b3b5fe05ed062)
+  set_git_default(MICM_GIT_TAG 4af29ff)
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
