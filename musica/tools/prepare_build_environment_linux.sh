ec6294d2836bc21bbd4088f9350d73d4b7410ae8
Kyle Shores <kyle.shores44@gmail.com>
2025-07-07 13:33:49 -0600

    disabling netcdf for i686

--- musica/tools/prepare_build_environment_linux.sh
+++ musica/tools/prepare_build_environment_linux.sh
@@ -2,16 +2,35 @@
 set -e
 set -x
 
-dnf -y update
-dnf install -y epel-release
-dnf search netcdf
-dnf install -y tree wget zip netcdf-devel netcdf-fortran-devel
-
 target_arch="$(uname -m)"
 echo "Detected target_arch: $target_arch"
 
+# Set package manager based on architecture
+# x86_64 and aarch64 use manylinux_2_28 (AlmaLinux 8) with dnf
+# i686 uses manylinux2014 (CentOS 7) with yum
+if [ "$target_arch" = "i686" ]; then
+  PKG_MGR="yum"
+else
+  PKG_MGR="dnf"
+fi
+
+echo "Using package manager: $PKG_MGR"
+
+# Install basic dependencies
+$PKG_MGR -y update
+
+# Handle EPEL repository setup based on architecture
 if [ "$target_arch" = "x86_64" ]; then
-  # Install CUDA 12.6 for x86_64 on AlmaLinux 8 (manylinux_2_28) - supports GCC 14
+  # For manylinux_2_28 (AlmaLinux 8), epel-release is required to get netcdf, for some reason
+  $PKG_MGR install -y epel-release
+  $PKG_MGR search netcdf
+  $PKG_MGR install -y netcdf-devel netcdf-fortran-devel
+fi
+
+$PKG_MGR install -y tree wget zip 
+
+if [ "$target_arch" = "x86_64" ]; then
+  # Install CUDA 12.8 for x86_64 on AlmaLinux 8 (manylinux_2_28) - supports GCC 14
   dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
   dnf install --setopt=obsoletes=0 -y \
       cuda-nvcc-12-8 \
