744881bf887a3fa124228540aba440d4a0c44f3a
Kyle Shores <kyle.shores44@gmail.com>
2025-07-08 10:36:25 -0600

    turning the version into a module attribute

--- musica/tuvx.py
+++ musica/tuvx.py
@@ -8,6 +8,10 @@ from . import backend
 
 _backend = backend.get_backend()
 
+# Module-level version attribute
+version = _backend._tuvx._get_tuvx_version() if backend.tuvx_available() else None
+
+
 class TUVX:
     """
     A Python interface to the TUV-x photolysis calculator.
@@ -15,10 +19,5 @@ class TUVX:
 
     def __init__(self):
         if not backend.tuvx_available():
-            raise ValueError("TUV-x backend is not available on this platform.")
-
-    @staticmethod
-    def version() -> str:
-        if not backend.tuvx_available():
-            raise ValueError("TUV-x backend is not available on this platform.")
-        return _backend._tuvx._get_tuvx_version()
+            raise ValueError(
+                "TUV-x backend is not available on this platform.")
