4e213b2382edbff1261c689132f81b3f458d3256
Kyle Shores <kyle.shores44@gmail.com>
2025-07-10 16:30:55 -0600

    adding tuvx api

--- include/musica/tuvx/tuvx.hpp
+++ include/musica/tuvx/tuvx.hpp
@@ -30,6 +30,13 @@ namespace musica
     /// @param error Error struct to indicate success or failure
     void Create(const char *config_path, GridMap *grids, ProfileMap *profiles, RadiatorMap *radiators, Error *error);
 
+    /// @brief Create an instance of tuvx from a configuration file only (simple interface)
+    /// All parameters (solar zenith angle, Earth-Sun distance, atmospheric profiles, etc.)
+    /// are read from the JSON configuration file, similar to the Fortran tuvx.F90 driver
+    /// @param config_path Path to configuration file
+    /// @throws std::runtime_error if operation fails
+    void CreateFromConfigOnly(const char *config_path);
+
     /// @brief Create a grid map. For now, this calls the interal tuvx fortran api, but will allow the change to c++ later on
     /// to be transparent to downstream projects
     /// @param error The error struct to indicate success or failure
@@ -75,11 +82,50 @@ namespace musica
     /// @return TUV-x version string
     static std::string GetVersion();
 
+    /// @brief Run the TUV-x photolysis calculator (simple interface)
+    /// All parameters come from the JSON configuration file. Returns the computed
+    /// photolysis rates and heating rates directly.
+    /// @param photolysis_rate_constants Output array for photolysis rates [s^-1] (layer, reaction)
+    /// @param heating_rates Output array for heating rates [K/s] (layer, reaction)
+    /// @throws std::runtime_error if operation fails
+    void RunFromConfig(double *const photolysis_rate_constants, double *const heating_rates);
+
+    /// @brief Get the number of photolysis reactions
+    /// @return Number of photolysis reactions
+    /// @throws std::runtime_error if operation fails
+    int GetPhotolysisRateCount();
+
+    /// @brief Get the number of heating rate types
+    /// @return Number of heating rate types
+    /// @throws std::runtime_error if operation fails
+    int GetHeatingRateCount();
+
+    /// @brief Get the number of vertical layers
+    /// @return Number of vertical layers
+    /// @throws std::runtime_error if operation fails
+    int GetNumberOfLayers();
+
+    /// @brief Get the number of solar zenith angle steps
+    /// @return Number of solar zenith angle steps
+    /// @throws std::runtime_error if operation fails
+    int GetNumberOfSzaSteps();
+
+    /// @brief Get photolysis rate names (simple interface)
+    /// @return Vector of photolysis rate names
+    /// @throws std::runtime_error if operation fails
+    std::vector<std::string> GetPhotolysisRateNames();
+
+    /// @brief Get heating rate names (simple interface)
+    /// @return Vector of heating rate names
+    /// @throws std::runtime_error if operation fails
+    std::vector<std::string> GetHeatingRateNames();
+
     ~TUVX();
 
    private:
     void *tuvx_;
     int number_of_layers_;
+    bool is_config_only_mode_;
   };
 
 }  // namespace musica
