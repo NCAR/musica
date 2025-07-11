da68e16170e75a853ac9e5c9c9ab4c8e1374a45f
Kyle Shores <kyle.shores44@gmail.com>
2025-07-10 20:56:21 -0600

    adding tuv config

--- musica/test/test_tuvx.py
+++ musica/test/test_tuvx.py
@@ -11,6 +11,22 @@ def test_tuvx_version():
     assert isinstance(version, str)
     print(f"TUV-x version: {version}")
 
+def test_tuvx_from_file(monkeypatch):
+    monkeypatch.chdir("configs/tuvx")
+    file = "tuv_5_4.json"
+    tuvx = musica.TUVX(file)
+    assert tuvx is not None
+
+    heating_rates = tuvx.heating_rate_names
+    photolysis_rates = tuvx.photolysis_rate_names
+    print(f"Heating rates: {heating_rates}")
+    print(f"Photolysis rates: {photolysis_rates}")
+    rates = tuvx.run()
+    print(f"Rates: {rates}")
+
+    assert len(heating_rates) > 0, "No heating rates found"
+    assert len(photolysis_rates) > 0, "No photolysis rates found"
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
