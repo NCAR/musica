f6c7ba993c05ffae3b94865164800a377e79f13a
Kyle Shores <kyle.shores44@gmail.com>
2025-07-10 21:09:39 -0600

    adding another test

--- musica/test/test_tuvx.py
+++ musica/test/test_tuvx.py
@@ -11,7 +11,7 @@ def test_tuvx_version():
     assert isinstance(version, str)
     print(f"TUV-x version: {version}")
 
-def test_tuvx_from_file(monkeypatch):
+def test_full_tuvx(monkeypatch):
     monkeypatch.chdir("configs/tuvx")
     file = "tuv_5_4.json"
     tuvx = musica.TUVX(file)
@@ -24,9 +24,26 @@ def test_tuvx_from_file(monkeypatch):
     rates = tuvx.run()
     print(f"Rates: {rates}")
 
+    assert len(rates) > 0, "No photolysis rates found"
+    assert len(photolysis_rates) > 0, "No photolysis rates found"
+
+
+def test_fixed_tuvx(monkeypatch):
+    monkeypatch.chdir("src")
+    file = "test/data/tuvx/fixed/config.json"
+    tuvx = musica.TUVX(file)
+    assert tuvx is not None
+
+    heating_rates = tuvx.heating_rate_names
+    photolysis_rates = tuvx.photolysis_rate_names
+    print(f"Heating rates: {heating_rates}")
+    print(f"Photolysis rates: {photolysis_rates}")
+
     assert len(heating_rates) > 0, "No heating rates found"
     assert len(photolysis_rates) > 0, "No photolysis rates found"
 
+    # rates = tuvx.run()
+    # print(f"Rates: {rates}")
 
 if __name__ == '__main__':
     pytest.main([__file__])
