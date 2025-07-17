0b01d7bd66248a5ddf3f6bc7c24e85dddadd49e9
Kyle Shores <kyle.shores44@gmail.com>
2025-07-17 14:42:37 -0500

    removing unnecessary change

--- musica/tools/prepare_build_environment_linux.sh
+++ musica/tools/prepare_build_environment_linux.sh
@@ -47,20 +47,4 @@ if [ "$target_arch" = "x86_64" ]; then
   
   # list the installed CUDA packages
   # tree -L 4 /usr/local/cuda
-elif [ "$target_arch" = "aarch64" ]; then
-  # Install CUDA 12.8 for aarch64 on AlmaLinux 8 (manylinux_2_28) - supports GCC 14
-  dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/aarch64/cuda-rhel8.repo
-  dnf install --setopt=obsoletes=0 -y \
-      cuda-nvcc-12-8 \
-      cuda-cudart-devel-12-8 \
-      libcurand-devel-12-8 \
-      libcublas-devel-12-8 
-  ln -sf cuda-12.8 /usr/local/cuda
-
-  # Verify CUDA installation
-  echo "=== CUDA Installation Verification ==="
-  /usr/local/cuda/bin/nvcc --version
-  
-  # list the installed CUDA packages
-  # tree -L 4 /usr/local/cuda
 fi
