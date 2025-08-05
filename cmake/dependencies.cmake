6caf6df6662ccfa2073767402962eb695b11169d
Matt Dawson <mattdawson@ucar.edu>
2025-08-05 08:03:33 -0700

    update mechanism configuration

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -25,7 +25,7 @@ endif()
 
 if(MUSICA_BUILD_C_CXX_INTERFACE)
   set_git_default(MECH_CONFIG_GIT_REPOSITORY https://github.com/NCAR/MechanismConfiguration.git)
-  set_git_default(MECH_CONFIG_GIT_TAG v0.2.0)
+  set_git_default(MECH_CONFIG_GIT_TAG ab615fe7a8768392db6cc54bec67d8f13166a085)
 
   FetchContent_Declare(mechanism_configuration
       GIT_REPOSITORY ${MECH_CONFIG_GIT_REPOSITORY}
--- musica/mechanism_configuration/species.py
+++ musica/mechanism_configuration/species.py
@@ -1,6 +1,7 @@
 from typing import Optional, Any, Dict
 from .. import backend
 from .utils import _add_other_properties, _remove_empty_keys
+from enum import Enum
 
 _backend = backend.get_backend()
 _Species = _backend._mechanism_configuration._Species
@@ -18,7 +19,9 @@ class Species(_Species):
         N_star (float): A parameter used to calculate the mass accomodation factor (Ervens et al., 2003)
         molecular_weight_kg_mol (float): Molecular weight [kg mol-1]
         density_kg_m3 (float): Density [kg m-3]
-        tracer_type (str): The type of tracer ("AEROSOL", "THIRD_BODY", "CONSTANT").
+        constant_concentration_mol_m3 (float): Constant concentration of the species (mol m-3)
+        constant_mixing_ratio_mol_mol (float): Constant mixing ratio of the species (mol mol-1)
+        is_third_body (bool): Whether the species is a third body.
         other_properties (Dict[str, Any]): A dictionary of other properties of the species.
     """
 
@@ -31,7 +34,9 @@ class Species(_Species):
         N_star: Optional[float] = None,
         molecular_weight_kg_mol: Optional[float] = None,
         density_kg_m3: Optional[float] = None,
-        tracer_type: Optional[str] = None,
+        constant_concentration_mol_m3: Optional[float] = None,
+        constant_mixing_ratio_mol_mol: Optional[float] = None,
+        is_third_body: Optional[bool] = False,
         other_properties: Optional[Dict[str, Any]] = None,
     ):
         """
@@ -45,7 +50,9 @@ class Species(_Species):
             N_star (float): A parameter used to calculate the mass accomodation factor (Ervens et al., 2003)
             molecular_weight_kg_mol (float): Molecular weight [kg mol-1]
             density_kg_m3 (float): Density [kg m-3]
-            tracer_type (str): The type of tracer ("AEROSOL", "THIRD_BODY", "CONSTANT").
+            constant_concentration_mol_m3 (float): Constant concentration of the species (mol m-3)
+            constant_mixing_ratio_mol_mol (float): Constant mixing ratio of the species (mol mol-1)
+            is_third_body (bool): Whether the species is a third body.
             other_properties (Dict[str, Any]): A dictionary of other properties of the species.
         """
         super().__init__()
@@ -56,7 +63,9 @@ class Species(_Species):
         self.N_star = N_star if N_star is not None else self.N_star
         self.molecular_weight_kg_mol = molecular_weight_kg_mol if molecular_weight_kg_mol is not None else self.molecular_weight_kg_mol
         self.density_kg_m3 = density_kg_m3 if density_kg_m3 is not None else self.density_kg_m3
-        self.tracer_type = tracer_type if tracer_type is not None else self.tracer_type
+        self.constant_concentration_mol_m3 = constant_concentration_mol_m3 if constant_concentration_mol_m3 is not None else self.constant_concentration_mol_m3
+        self.constant_mixing_ratio_mol_mol = constant_mixing_ratio_mol_mol if constant_mixing_ratio_mol_mol is not None else self.constant_mixing_ratio_mol_mol
+        self.is_third_body = is_third_body if is_third_body is not None else self.is_third_body
         self.other_properties = other_properties if other_properties is not None else self.other_properties
 
     @staticmethod
@@ -69,7 +78,9 @@ class Species(_Species):
             "N star": instance.N_star,
             "molecular weight [kg mol-1]": instance.molecular_weight_kg_mol,
             "density [kg m-3]": instance.density_kg_m3,
-            "tracer type": instance.tracer_type,
+            "constant concentration [mol m-3]": instance.constant_concentration_mol_m3,
+            "constant mixing ratio [mol mol-1]": instance.constant_mixing_ratio_mol_mol,
+            "is third body": instance.is_third_body,
         }
         _add_other_properties(serialize_dict, instance.other_properties)
         return _remove_empty_keys(serialize_dict)
