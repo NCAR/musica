6a951230a900dd6826cc4ef8a7d67ae97fd626eb
Kyle Shores <kyle.shores44@gmail.com>
2025-08-18 16:16:40 -0500

    trying to build on linux

--- musica/tools/prepare_build_environment_linux.sh
+++ musica/tools/prepare_build_environment_linux.sh
@@ -5,17 +5,18 @@ set -x
 target_arch="$(uname -m)"
 echo "Detected target_arch: $target_arch"
 
-# Set package manager based on architecture
-# x86_64 and aarch64 use manylinux_2_28 (AlmaLinux 8) with dnf
-# i686 uses manylinux2014 (CentOS 7) with yum
-if [ "$target_arch" = "i686" ]; then
+# Detect the Linux distribution and set package manager
+# Check if we're in a CentOS 7 environment (old manylinux2014) or AlmaLinux 8 (manylinux_2_28)
+if [ -f /etc/centos-release ] && grep -q "CentOS Linux release 7" /etc/centos-release; then
+  # CentOS 7 (manylinux2014) - uses yum
   PKG_MGR="yum"
   
-  # CentOS 7 is EOL, so we need to use vault.centos.org for i686 builds
+  # CentOS 7 is EOL, so we need to use vault.centos.org for old builds
   # Replace the repo files to point to vault.centos.org
   sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*.repo
   sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*.repo
 else
+  # AlmaLinux 8 or newer (manylinux_2_28) - uses dnf
   PKG_MGR="dnf"
 fi
 
@@ -23,8 +24,9 @@ echo "Using package manager: $PKG_MGR"
 
 $PKG_MGR -y update
 
-if [ "$target_arch" = "x86_64" ] || [ "$target_arch" = "aarch64" ]; then
-  # For manylinux_2_28 (AlmaLinux 8), epel-release is required to get netcdf, for some reason
+# Install NetCDF if we're on a modern system (manylinux_2_28)
+if [ "$PKG_MGR" = "dnf" ]; then
+  # For manylinux_2_28 (AlmaLinux 8), epel-release is required to get netcdf
   $PKG_MGR install -y epel-release
   $PKG_MGR install -y netcdf-devel netcdf-fortran-devel
 fi
