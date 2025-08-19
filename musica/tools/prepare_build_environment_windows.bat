b9d8b7d83ff3e890a1d16d3525d7112869169e5b
Kyle Shores <kyle.shores44@gmail.com>
2025-08-19 07:52:50 -0500

    maybe this

--- musica/tools/prepare_build_environment_windows.bat
+++ musica/tools/prepare_build_environment_windows.bat
@@ -1,9 +1,6 @@
 @echo off
 REM Windows build environment preparation script for MUSICA
 
-REM Ensure MSYS2/MinGW64 is installed and in PATH
-echo Setting up MinGW64 build environment...
-
 REM Add MSYS2 MinGW64 to PATH if not already there
 set "MINGW_PATH=C:\msys64\mingw64\bin"
 echo %PATH% | find /i "%MINGW_PATH%" >nul || set "PATH=%MINGW_PATH%;%PATH%"
@@ -22,8 +19,3 @@ g++ --version
 
 echo GFortran version:
 gfortran --version
-
-REM Set CMake to use MinGW Makefiles generator
-set CMAKE_GENERATOR=MSYS Makefiles
-
-echo MinGW64 build environment setup complete.
