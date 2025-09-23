bf7dbf4c09534af6c4460bc09da0d80e96d25373
Jiwon Gim <jiwongim@ucar.edu>
2025-09-22 20:23:43 -0600

    update the micm tag

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -75,7 +75,8 @@ endif()
 
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE)
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG 9f773c85da99bfeec22656d47c55f8e7500ee613)
+  # set_git_default(MICM_GIT_TAG 9f773c85da99bfeec22656d47c55f8e7500ee613)
+  set_git_default(MICM_GIT_TAG investigate_musica_error)
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
