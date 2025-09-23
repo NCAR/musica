14c3066ae5da860fa96c304c3e77230048dd5cd5
Blvckjs96 <kiet.nguyen3896@gmail.com>
2025-09-22 22:40:59 -0500

    mininal javascript build and version API functionality

---
+++ javascript/src/musica_wrapper.h
@@ -0,0 +1,27 @@
+#ifndef MUSICA_WRAPPER_H
+#define MUSICA_WRAPPER_H
+
+#include <string>
+#include <memory>
+
+namespace musica_addon {
+
+/**
+ * @brief C++ wrapper for MUSICA functionality
+ */
+class MusicaWrapper {
+public:
+    MusicaWrapper();
+    ~MusicaWrapper();
+
+    // Version and build information
+    std::string GetVersion() const;
+
+    private:
+    // Internal implementation details
+    class Impl;
+    std::unique_ptr<Impl> pImpl_;
+};
+}
+
+#endif // MUSICA_WRAPPER_H
