8354edf60a6f53bbf0e5c1abd897bf406e78aa37
Kyle Shores <kyle.shores44@gmail.com>
2025-09-28 13:13:12 -0500

    correcting test

--- src/test/unit/micm/parser.cpp
+++ src/test/unit/micm/parser.cpp
@@ -113,7 +113,7 @@ TEST(Parser, CanParseFullV1)
     musica::Chemistry chemistry = musica::ReadConfiguration("configs/v1/full_configuration/full_configuration" + extension);
     EXPECT_EQ(chemistry.system.gas_phase_.species_.size(), 5);
     EXPECT_EQ(chemistry.system.gas_phase_.name_, "gas");
-    EXPECT_EQ(chemistry.system.phases_.size(), 3);
+    EXPECT_EQ(chemistry.system.phases_.size(), 0);
     EXPECT_EQ(chemistry.processes.size(), 12);
   }
 }
@@ -283,7 +283,6 @@ TEST(Parser, ConvertSurfaceV0ToV1)
   EXPECT_EQ(v1_mechanism.reactions.surface.size(), 1);
   EXPECT_EQ(v1_mechanism.reactions.surface[0].name, "test_surface");
   EXPECT_EQ(v1_mechanism.reactions.surface[0].gas_phase, "gas");
-  EXPECT_EQ(v1_mechanism.reactions.surface[0].condensed_phase, "condensed");
   EXPECT_NEAR(v1_mechanism.reactions.surface[0].reaction_probability, 0.1, 1e-10);
   EXPECT_EQ(v1_mechanism.reactions.surface[0].gas_phase_species.species_name, "A");
   EXPECT_EQ(v1_mechanism.reactions.surface[0].gas_phase_products.size(), 1);
