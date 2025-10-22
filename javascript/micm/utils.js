0b646299e3b9dd875bf50d7c044f1b2875459ae3
miles-q-ryder <falconfire1405@gmail.com>
2025-10-22 08:26:36 -0500

    minor bug fixes

--- javascript/micm/utils.js
+++ javascript/micm/utils.js
@@ -5,4 +5,9 @@ function isScalarNumber(x) {
 		!(typeof x === 'boolean')
 	);
 }
-module.exports = { isScalarNumber };
+
+const AVOGADRO = 6.02214076e23  // mol^-1
+const BOLTZMANN = 1.380649e-23  // J K^-1
+const GAS_CONSTANT = AVOGADRO * BOLTZMANN  // J K^-1 mol^-1
+
+module.exports = { isScalarNumber, AVOGADRO, BOLTZMANN, GAS_CONSTANT };
