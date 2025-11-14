528d3a171011213e4c0c3c3eb463661d083c5838
Matt Dawson <matt@cohere-llc.com>
2025-11-14 09:51:25 -0800

    add radiator and radiator map to python API

--- python/musica/tuvx/__init__.py
+++ python/musica/tuvx/__init__.py
@@ -3,3 +3,5 @@ from .grid_map import GridMap
 from .grid import Grid
 from .profile_map import ProfileMap
 from .profile import Profile
+from .radiator_map import RadiatorMap
+from .radiator import Radiator
