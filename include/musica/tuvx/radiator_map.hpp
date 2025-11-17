4a59e487d65b3f1d149fdc77a7dbc37eb8af0027
Matt Dawson <matt@cohere-llc.com>
2025-11-17 12:12:52 -0800

    fix copyright statement

--- include/musica/tuvx/radiator_map.hpp
+++ include/musica/tuvx/radiator_map.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2023-2025 National Center for Atmospheric Research
+// Copyright (C) 2023-2025 University Corporation for Atmospheric Research
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
