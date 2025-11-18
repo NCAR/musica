26fb4b3e94eacb1cc18c74fb90ac19f332b94497
Kyle Shores <kyle.shores44@gmail.com>
2025-11-18 12:59:26 -0600

    removing debug build

--- javascript/micm/micm.js
+++ javascript/micm/micm.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const addon = require(path.join(
 	__dirname,
-	'../../build/Debug/musica-addon.node'
+	'../../build/Build/musica-addon.node'
 ));
 
 const { State } = require('./state.js');
