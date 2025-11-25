29cc23e11d6d016425e7cf07d40fda5d519085c7
Kyle Shores <kyle.shores44@gmail.com>
2025-11-25 09:22:46 -0600

    updating tuvx tag

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -104,7 +104,7 @@ if (MUSICA_ENABLE_TUVX AND MUSICA_BUILD_C_CXX_INTERFACE)
   set(TUVX_INSTALL_INCLUDE_DIR ${MUSICA_INSTALL_INCLUDE_DIR} CACHE STRING "" FORCE)
 
   set_git_default(TUVX_GIT_REPOSITORY https://github.com/NCAR/tuv-x.git)
-  set_git_default(TUVX_GIT_TAG remove_warnings)
+  set_git_default(TUVX_GIT_TAG de68a877920ad2404f660446205fc1bd21a35ea5)
 
   FetchContent_Declare(tuvx
     GIT_REPOSITORY ${TUVX_GIT_REPOSITORY}
