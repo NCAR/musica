484bbb95fc8df65f94f7db06160a2be294764533
Kyle Shores <kyle.shores44@gmail.com>
2025-12-01 13:21:08 -0600

    Adding solver stats, updating binding finding
    - Solver stats are now returned to javascript
    - The binding is now imported using the bindings package, so switching
      between debug/release no longer requires changes to the code
    - added micm tests

--- javascript/src/micm/micm_wrapper.h
+++ javascript/src/micm/micm_wrapper.h
@@ -1,32 +1,39 @@
 #pragma once
 
-#include <string>
-#include <memory>
-#include <map>
-#include <vector>
 #include "state_wrapper.h"
+
 #include <musica/micm/micm.hpp>
 
+#include <micm/solver/solver_result.hpp>
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
 // Forward declarations of MUSICA types
-namespace musica {
-    class State;
+namespace musica
+{
+  class State;
 }
 
-namespace musica_addon {
+namespace musica_addon
+{
 
-/// @brief C++ wrapper for MICM solver
-class MICMWrapper {
-public:
+  /// @brief C++ wrapper for MICM solver
+  class MICMWrapper
+  {
+   public:
     MICMWrapper(const std::string& config_path, int solver_type);
     ~MICMWrapper();
 
     musica::State* CreateState(size_t number_of_grid_cells);
-    void Solve(musica::State* state, double time_step);
+    micm::SolverResult Solve(musica::State* state, double time_step);
     int GetSolverType() const;
 
-private:
+   private:
     musica::MICM* micm_;
     int solver_type_;
-};
+  };
 
-}
+}  // namespace musica_addon
