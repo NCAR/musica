ee8cd7b757440ef17e3fe0d3de0f11b56e9c63a1
Jiwon Gim <jiwongim@ucar.edu>
2026-01-12 16:15:28 -0700

    update the year

--- include/musica/tuvx/radiator.hpp
+++ include/musica/tuvx/radiator.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2023-2025 University Corporation for Atmospheric Research
+// Copyright (C) 2023-2026 University Corporation for Atmospheric Research
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
