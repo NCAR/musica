ef730d002060a63e0643ce28149af1a7c046b510
Kyle Shores <kyle.shores44@gmail.com>
2026-01-28 14:37:30 -0600

    remove option that is redundant

--- python/musica/main.py
+++ python/musica/main.py
@@ -50,12 +50,6 @@ def parse_arguments():
         type=str,
         help='Path to a musica v0 configuration to convert to v1 format.'
     )
-    parser.add_argument(
-        '--no-convert-units',
-        action='store_false',
-        dest='convert_units',
-        help='Do not convert units from molec-1 cm3 to mol-1 m3 when converting v0 configuration to v1 format. Only meaningful with --convert.'
-    )
     return parser.parse_args()
 
 
@@ -82,13 +76,15 @@ def copy_example(logger, example_arg, output_path):
         shutil.copy(example_path, output_path)
 
 
-def convert_configuration(logger, configuration, output_path, convert_units=True):
+def convert_configuration(logger, configuration, output_path):
     logger.info(f"Converting configuration: {configuration} to {output_path}")
     if not os.path.exists(configuration):
         raise ValueError(f"Configuration file '{configuration}' does not exist.")
 
     parser = musica.mechanism_configuration.Parser()
-    mechanism = parser.parse_and_convert_v0(configuration, convert_units)
+    # the mechanism configuration automatically converts pre exponential factors to SI units
+    # so we don't need to convert units again here.
+    mechanism = parser.parse_and_convert_v0(configuration, convert_units=False)
     mechanism.export(output_path)
 
 
@@ -121,7 +117,7 @@ def main():
         output = '.'
 
     if convert:
-        convert_configuration(logger, convert, output, args.convert_units)
+        convert_configuration(logger, convert, output)
     else:
         copy_example(logger, example_arg, output)
 
