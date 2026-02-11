19385e48b077b521a959623f1283371ba04ddee0
Kyle Shores <kyle.shores44@gmail.com>
2026-02-11 15:30:49 -0600

    removing ununsed option

--- python/musica/micm/micm.py
+++ python/musica/micm/micm.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from __future__ import annotations
-from typing import Union, Any, TYPE_CHECKING, List, Optional
+from typing import Union, Any, TYPE_CHECKING, Optional
 from os import PathLike
 
 from .state import State
