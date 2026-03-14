5ce469e046fd365e238bc21118b96bafb7773261
Kyle Shores <kyle.shores44@gmail.com>
2026-03-13 21:00:43 -0500

    newer newer mech config

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -26,7 +26,7 @@ endif()
 
 if(MUSICA_BUILD_C_CXX_INTERFACE AND NOT MUSICA_USE_PREBUILT)
   set_git_default(MECH_CONFIG_GIT_REPOSITORY https://github.com/NCAR/MechanismConfiguration.git)
-  set_git_default(MECH_CONFIG_GIT_TAG db98247f56e35016837fe0749300dffee15732c3)
+  set_git_default(MECH_CONFIG_GIT_TAG 73f548fca25d8179ef56f48b313478b2aec7c672)
 
   FetchContent_Declare(mechanism_configuration
       GIT_REPOSITORY ${MECH_CONFIG_GIT_REPOSITORY}
