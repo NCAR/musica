9a3561ce21ea4bafcb6d1b8ca740e9ecd4548fcc
Jian Sun <sunjian@ucar.edu>
2026-03-16 13:21:51 -0600

    Update MICM GIT_TAG to micm_claude_work branch commit

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -73,7 +73,7 @@ endif()
 
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE AND NOT MUSICA_USE_PREBUILT)
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG e854d2cf4d0abc7c6fb243e5406e751c462f3679)
+  set_git_default(MICM_GIT_TAG 974abc0b9d7fa0b05d434d7fed86eff9d140ea34)
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
