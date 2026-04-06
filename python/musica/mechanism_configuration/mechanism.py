44420988a362de7fafea0ca466b54d95dbb6fd9b
Kyle Shores <kyle.shores44@gmail.com>
2026-04-06 12:45:41 -0600

    defaulting version to 1.0.0

--- python/musica/mechanism_configuration/mechanism.py
+++ python/musica/mechanism_configuration/mechanism.py
@@ -61,7 +61,7 @@ class Mechanism(CppWrapper):
         self.species = species if species is not None else []
         self.phases = phases if phases is not None else []
         self.reactions = Reactions(reactions=reactions if reactions is not None else [])
-        self.version = version if version is not None else Version()
+        self.version = version if version is not None else Version(1, 0, 0)
 
     @property
     def species(self) -> List[Species]:
