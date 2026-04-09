363dfc40773fd06bbb75ebcd894cd4523175c57d
Matt Dawson <matt@cohere-llc.com>
2026-04-09 12:31:42 -0700

    add miam to build

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -73,7 +73,7 @@ endif()
 
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE AND NOT MUSICA_USE_PREBUILT)
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG fab5fffb9e1af8f196723c09016e51827118d9e6)
+  set_git_default(MICM_GIT_TAG 9d9167393d67cd6a3d74a7f46ff774cfb08f7f79)
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
