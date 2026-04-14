e39bb3341809f928ef128193338a74ba0b53f58e
Jian Sun <sunjian@ucar.edu>
2026-04-13 21:34:31 -0600

    Update MICM dependency to micm_claude_work branch for AddForcingTerms export

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -73,7 +73,7 @@ endif()
 
 if (MUSICA_ENABLE_MICM AND MUSICA_BUILD_C_CXX_INTERFACE AND NOT MUSICA_USE_PREBUILT)
   set_git_default(MICM_GIT_REPOSITORY https://github.com/NCAR/micm.git)
-  set_git_default(MICM_GIT_TAG e854d2cf4d0abc7c6fb243e5406e751c462f3679)
+  set_git_default(MICM_GIT_TAG 29da3b82091ca15ef051f89617bb8cb19ca88b1a)
 
   FetchContent_Declare(micm
       GIT_REPOSITORY ${MICM_GIT_REPOSITORY}
