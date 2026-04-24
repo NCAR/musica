a6fb45f4c9805412f197388327253e92e72b09e6
zmoon <zmoon92@gmail.com>
2026-04-24 16:21:13 -0500

    Fix remaining
    Bui is a paper co-author
    
    did rename a Fortran func, but it doesn't seem to be used anywhere?

--- include/musica/tuvx/radiator_map.hpp
+++ include/musica/tuvx/radiator_map.hpp
@@ -33,7 +33,7 @@ namespace musica
     /// @param error Error to indicate success or failure
     void AddRadiator(Radiator *radiator, Error *error);
 
-    /// @brief Returns a radiator. For now, this calls the interal tuvx fortran api, but will allow the change to c++ later
+    /// @brief Returns a radiator. For now, this calls the internal tuvx fortran api, but will allow the change to c++ later
     /// on to be transparent to downstream projects
     /// @param radiator_name Radiator name
     /// @param error Error to indicate success or failure
