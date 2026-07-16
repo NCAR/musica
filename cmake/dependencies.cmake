2a1b4e6c0a44bf4692399d84945b595002406fc3
Jiwon Gim <jiwongim@ucar.edu>
2026-07-16 11:19:06 -0600

    update the tag

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -67,7 +67,7 @@ endif()
 
 if(MUSICA_BUILD_C_CXX_INTERFACE AND NOT MUSICA_USE_PREBUILT)
   set_git_default(MECH_CONFIG_GIT_REPOSITORY https://github.com/NCAR/MechanismConfiguration.git)
-  set_git_default(MECH_CONFIG_GIT_TAG incorporate-feedback)
+  set_git_default(MECH_CONFIG_GIT_TAG a158c2d373662efb60ef9a2d7693ff8d01a809d5)
 
   FetchContent_Declare(mechanism_configuration
       GIT_REPOSITORY ${MECH_CONFIG_GIT_REPOSITORY}
